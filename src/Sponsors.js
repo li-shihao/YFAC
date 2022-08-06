f8e7e2033feca18465a9e65a8e7b5904895743c1
Arthur <me@shihao.li>
2022-08-06 21:23:34 +0800

    test

--- src/Sponsors.js
+++ src/Sponsors.js
@@ -6,10 +6,18 @@ export default function Sponsors() {
           2022 Sponsors
         </h3>
         <div className="flex flex-row mt-14 justify-center gap-14 md:gap-60 lg:gap-36 w-full lg:w-auto lg:mt-0">
-          <a href="https://www.theeconomicstutor.com/" target="_blank">
+          <a
+            class="w-full h-full"
+            href="https://www.theeconomicstutor.com/"
+            target="_blank"
+          >
             <img class="max-h-20" src={require("./Tutor.png")}></img>
           </a>
-          <a href="https://www.thebaummovement.org/" target="_blank">
+          <a
+            class="w-full h-full"
+            href="https://www.thebaummovement.org/"
+            target="_blank"
+          >
             <div className="flex flex-row items-center">
               <img class="max-h-20" src={require("./Baum.png")}></img>
               <span class="text-emerald-900 font-bold text-2xl ml-2">BAUM</span>
