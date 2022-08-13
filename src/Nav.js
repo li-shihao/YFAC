e4ae391c0ffb40a25d8ad1acc06e791c09a2d4ec
Arthur <me@shihao.li>
2022-08-13 22:37:10 +0800

    corrected mobile nav link

--- src/Nav.js
+++ src/Nav.js
@@ -68,12 +68,12 @@ export default function Nav() {
               </a>
             </Link>
             <Link to="/judges">
-            <a
-              href="#"
-              className="text-base font-medium text-gray-500 hover:text-gray-900"
-            >
-              Judges
-            </a>
+              <a
+                href="#"
+                className="text-base font-medium text-gray-500 hover:text-gray-900"
+              >
+                Judges
+              </a>
             </Link>
             <Link to="/faq">
               <a
@@ -167,7 +167,7 @@ export default function Nav() {
                     Competition Guidelines
                   </a>
                 </Link>
-                <Link to="/">
+                <Link to="/judges">
                   <a
                     href="#"
                     className="text-base font-medium text-gray-900 hover:text-gray-700"
